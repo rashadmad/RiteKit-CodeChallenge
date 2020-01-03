e5b6c4211ba291914390685d2461545b5fa58e77
rashad <rashadmad@gmail.com>
2020-01-03 11:46:41 -0600

    add form and twiter form

--- code-challenge/src/App.vue
+++ code-challenge/src/App.vue
@@ -1,37 +1,105 @@
 <template>
-    <v-app>
-  <v-navigation-drawer app>
-    <!-- -->
-  </v-navigation-drawer>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-  <v-app-bar app>
-    <!-- -->
-  </v-app-bar>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
-  <v-content>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col>
+            <v-card class="mx-auto" max-width="344">
+              <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">be•nev•o•lent</p>
+                <p>adjective</p>
+                <div class="text--primary">
+                  well meaning and kindly.
+                  <br />"a benevolent smile"
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="deep-purple accent-4">Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+              <v-card-title>
+                <v-icon large left>mdi-twitter</v-icon>
+                <span class="title font-weight-light">Twitter</span>
+              </v-card-title>
 
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-      <HelloWorld></HelloWorld>
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-content>
+              <v-card-text
+                class="headline font-weight-bold"
+              >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
 
-  <v-footer app>
-    <!-- -->
-  </v-footer>
-</v-app>
+              <v-card-actions>
+                <v-list-item class="grow">
+                  <v-list-item-avatar color="grey darken-3">
+                    <v-img
+                      class="elevation-6"
+                      src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                    ></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title>Evan You</v-list-item-title>
+                  </v-list-item-content>
+
+                  <v-row align="center" justify="end">
+                    <v-icon class="mr-1">mdi-heart</v-icon>
+                    <span class="subheading mr-2">256</span>
+                    <span class="mr-1">·</span>
+                    <v-icon class="mr-1">mdi-share-variant</v-icon>
+                    <span class="subheading">45</span>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
+  props: {
+    source: String
+  },
   data: () => ({
-    //
+    drawer: null
   }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
 };
 </script>
